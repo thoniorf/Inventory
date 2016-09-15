96ea2bb6e62359fa428a527ea2a2198cb351643f
Antonio Fortino <thoniorf@gmail.com>
2016-09-15 22:48:26 +0200

    Added application icon,css rules and semantic html
    Added application icon to assets folder
    
    Added style rules for index page.
    
    Semantic html.

--- main.js
+++ main.js
@@ -1,41 +1,54 @@
-const {app, BrowserWindow} = require('electron');
+/* jshint esversion: 6 */
+
+const {
+    app,
+    BrowserWindow
+} = require('electron');
 
 let win;
 
- // This method will be called when Electron has finished
+// This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-  win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({
+        name: "Inventory",
+        width: 800,
+        height: 600,
+        toolbar: false,
+        icon: `${__dirname}/app/assets/bag.png`
+    });
+    win.setMenu(null);
+    
 
-   // and load the index.html of the app.
-   win.loadURL(`file://${__dirname}/app/index.html`)
+    // and load the index.html of the app.
+    win.loadURL(`file://${__dirname}/app/index/index.html`);
 
-   // Open the DevTools.
-  // win.webContents.openDevTools()
+    // Open the DevTools.
+    win.webContents.openDevTools();
 
-   // Emitted when the window is closed.
-   win.on('closed', () => {
-     // Dereference the window object, usually you would store windows
-     // in an array if your app supports multi windows, this is the time
-     // when you should delete the corresponding element.
-     win = null
-   })
-})
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
+});
